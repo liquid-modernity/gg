f0d28cbebbdbbfbddae4c408003bfc06b59a48c5
liquid-modernity <ulunkgates@gmail.com>
2026-02-07 18:32:18 +0700

    release: update artifacts

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* public/sw.js — deterministic updates + offline fallback */
-const VERSION = "3666fa4";
+const VERSION = "77d923e";
 const CACHE_STATIC = `gg-static-${VERSION}`;
 const CACHE_RUNTIME = `gg-runtime-${VERSION}`;
 
