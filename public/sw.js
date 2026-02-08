38641da13fb7c45572c969f3b3ba0629262b99ad
liquid-modernity <ulunkgates@gmail.com>
2026-02-08 13:54:18 +0700

    chore: update work

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* public/sw.js — deterministic updates + offline fallback */
-const VERSION = "a6766a0";
+const VERSION = "c0dd439";
 const CACHE_STATIC = `gg-static-${VERSION}`;
 const CACHE_RUNTIME = `gg-runtime-${VERSION}`;
 
