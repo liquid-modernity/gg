f9bbeb9890e43001ea702478a8e517c02873efc3
liquid-modernity <ulunkgates@gmail.com>
2026-02-13 23:11:28 +0700

    release: update artifacts

--- src/worker.js
+++ src/worker.js
@@ -150,7 +150,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const { pathname } = url;
-    const WORKER_VERSION = "90433f2";
+    const WORKER_VERSION = "721946f";
     const stamp = (res, opts = {}) => {
       const h = new Headers(res.headers);
       h.set("X-GG-Worker", "proxy");
